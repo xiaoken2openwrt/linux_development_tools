9192201aaa2a5a6e1c8cb3b1bc5fe879ab28e5b2
augus.sun <augus.sun@lockin.com>
2023-07-28 21:00:19 +0800

    安装clang并增加pre-commit自动格式化脚本

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ sudo apt-get install p7zip-full
 sudo apt-get install lzop
 sudo apt-get install lib32z1
 #sudo apt-get install lib32ncurses5(ubuntu20应该是libncurses6)
+sudo apt-get install clang-format-10
+sudo ln -s /usr/bin/clang-format-10 /usr/bin/clang-format
 sudo apt-get install ctags
 sudo apt-get install cscope
 sudo apt-get install ripgrep
