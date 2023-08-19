74cb6544d7252d6e9224632fe6edae6e991e6e2b
augus.sun <augus.sun@google.com>
2023-08-19 12:16:59 +0800

    增加cscope从.ctagsignore过滤文件的功能

--- downloads/cc.sh
+++ downloads/cc.sh
@@ -1,28 +1,38 @@
 #!/bin/sh
 
-ignorefile=`cat .ctagsignore `
-echo ${ignorefile}
+ignoreitem=`cat .ctagsignore `
+echo ${ignoreitem}
 
 ignorepath=""
-for p in ${ignorefile}
+ignorefile=""
+for p in ${ignoreitem}
 do
-#    if [ ! ${ignorepath[0]} ]; then
-#        ignorepath="${ignorepath} -path ${p}"
-#    else
-#        ignorepath="${ignorepath} -o -path ${p}"
-#    fi
-    ignorepath=${ignorepath}" -o -path ./"${p}
+    if [ -d ${p} ]; then
+		ignorepath=${ignorepath}" -o -path ./"${p}
+	elif [ -f ${p} ];then
+        ignorefile="${ignorefile} ./${p}"
+	else
+		echo "not match ${p}"
+	fi
+#    ignorepath=${ignorepath}" -o -path ./"${p}
 done
 
 echo ${ignorepath}
 ignorepath=${ignorepath:3}
 echo ${ignorepath}
+echo ${ignorefile}
 
 #find . \( -path './.git' -o  -path './ci' \) -prune -o -print  >cscope.file
 #find . \( ${ignorepath} \) -prune -o -print  >cscope.files
 #find . \( ${ignorepath} \) -prune -o -type f -name *.c -print  >cscope.files
 find . \( ${ignorepath} \) -prune -o -type f \( -name "*.cpp" -o -name "*.cc" -o -name "*.c" -o -name "*.h" \) -print  >cscope.files
 
+#exclude file
+for p in ${ignorefile}
+do
+	grep -v "${p}"  cscope.files >cscope.temp
+	mv cscope.temp cscope.files
+done
 
 #cscope -Rbkq;ctags -R --exclude=@.ctagsignore  *
 cscope -bkq -i cscope.files;ctags -R --exclude=@.ctagsignore  *
