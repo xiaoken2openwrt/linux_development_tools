93e654d9f161808fbaac2b2b55e0827a58d9ef73
augus.sun <augus.sun@wtcy.com>
2026-08-30 19:30:11 +0800

    增加python3-dev的工具,避免ubuntu-24.04缺少python环境导致ycm报错的问题

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo ehco "start install base tools..."
 sudo apt-get install python2.7-dev       #(ubuntu22.04 or higher version  default no python2,so must manual install it)
+sudo apt-get install python3-dev python3.12-dev  #(ubuntu24.04 default python3,so must manual install develop version)
 sudo apt-get install pkg-config
 sudo apt-get install ssh
 sudo apt-get install vim
